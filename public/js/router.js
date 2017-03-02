d373101cb18f405c91a3dfcf04044b7910ad6ae2
mattmartinez <mattmartinez816@gmail.com>
2017-03-02 10:40:19 -0800

    updated router.js

--- public/js/router.js
+++ public/js/router.js
@@ -1,1 +1,17 @@
 angular.module('movieReviewApp', ['ui.router'])
+  .config(AppRouter)
+  // .config(authInterceptor)
+
+  // function authInterceptor($httpProvider) {
+  //   $httpProvider.interceptors.push('AuthInterceptor')
+  // }
+
+  function AppRouter($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    .state('index', {
+      url: '/'
+    })
+  }
