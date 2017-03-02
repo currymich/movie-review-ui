b65ec20cd0e98efa41b9d7fb7f169c1043a0fb2a
mattmartinez <mattmartinez816@gmail.com>
2017-03-02 10:56:11 -0800

    Updated Auth Controller

---
+++ public/js/controllers/auth_controller.js
@@ -0,0 +1,34 @@
+function AuthController($http, $state, $scope, $rootScope, AuthTokenFactory) {
+  var self    = this;
+  var server  = 'http://localhost:3000';
+
+  function signup(userPass) {
+    $http.post(`${server}/users`, { user: userPass })
+      .then(function(response) {
+        // $state.go('login');
+      });
+  }
+
+  function login(userPass) {
+    $http.post(`${server}/users/login`, { user: userPass })
+      .then(function(response) {
+        console.log(response.data)
+        AuthTokenFactory.setToken(response.data.token)
+
+        $scope.$emit('userLoggedIn', response.data.user);
+        $rootScope.$emit('fetchData', response.data.user);
+        // $state.go('index');
+      });
+  }
+
+  function logout() {
+    AuthTokenFactory.setToken()
+
+    $scope.$emit('userLoggedOut');
+    // $state.go('index');
+  }
+
+  this.signup = signup;
+  this.login = login;
+  this.logout = logout;
+}
