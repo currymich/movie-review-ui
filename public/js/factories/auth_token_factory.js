9f06b9a36c22b2e20b617c44af825eddf4a5a969
mattmartinez <mattmartinez816@gmail.com>
2017-03-02 11:57:00 -0800

    Updated AuthTokenFactory

---
+++ public/js/factories/auth_token_factory.js
@@ -0,0 +1,21 @@
+function AuthTokenFactory($window) {
+  var store = $window.localStorage;
+  var key = 'auth-token';
+
+  return {
+    getToken: getToken,
+    setToken: setToken
+  }
+
+  function getToken() {
+    return store.getItem(key)
+  }
+
+  function setToken(token) {
+    if (token) {
+      store.setItem(key, token)
+    } else {
+      store.removeItem(key)
+    }
+  }
+}
