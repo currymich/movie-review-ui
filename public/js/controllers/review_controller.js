bcc2ad0eb9a842205143080c5a6dfb25bb0d469d
mattmartinez <mattmartinez816@gmail.com>
2017-03-03 12:11:12 -0800

    Added movie review function

--- public/js/controllers/review_controller.js
+++ public/js/controllers/review_controller.js
@@ -2,7 +2,7 @@ function ReviewController($scope, $http) {
   var self = this;
   server = 'http://localhost:3000'
   self.allReviews = [];
-
+  // self.movieReviews = [];
 
   function getAllReviews() {
     $http.get(`${server}/reviews`)
@@ -14,10 +14,15 @@ function ReviewController($scope, $http) {
   }
   getAllReviews();
 
-  function getMovieReviews() {
+  // function getMovieReviews() {
+  //   $http.get(`${server}/movies/${id}/reviews`)
+  //     .then(function(reviewResponse) {
+  //       console.log(reviewResponse)
+  //       console.log(reviewResponse.data)
+  //       self.movieReviews = reviewResponse.data;
+  //     });
+  // }
 
-  }
-
-
+  // self.movieReviews = movieReviews;
   self.getAllReviews = getAllReviews;
 }
