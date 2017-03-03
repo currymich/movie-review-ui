7975445d7df686aee2d00011d2e2b7626728624c
currymich <currymich@gmail.com>
2017-03-03 00:37:24 -0800

    displays data and image for single result, just image for 2,3,4

--- public/js/controllers/movie_controller.js
+++ public/js/controllers/movie_controller.js
@@ -1,11 +1,18 @@
 function MovieController($scope, $http) {
   var self = this;
   server = 'http://localhost:3000'
+  self.result1 = []
+  self.result1.Poster = ''
 
   function searchMovie(){
     $http.get(`${server}/movies/search?title=${self.title}`)
     .then(function(response){
-      console.log(response.data.movie)
+      self.bigResult = response.data.large
+      self.result1 = response.data.small.Search[1]
+      self.result2 = response.data.small.Search[2]
+      self.result3 = response.data.small.Search[3]
+      console.log(response.data.small)
+      console.log(response.data.large)
     })
   }
 
