bbdef8e09ed4308394690b1e33e86b57ed59f5cb
currymich <currymich@gmail.com>
2017-03-06 08:52:04 -0800

    added imdbID as param, removed console logs, updated getAll to getUsers or getMovieReviews where appropriate

--- public/js/controllers/review_controller.js
+++ public/js/controllers/review_controller.js
@@ -1,53 +1,71 @@
 function ReviewController($scope, $http) {
   var self = this;
   server = 'http://localhost:3000'
-  self.allReviews = [];
+
+  $scope.$on('newSearch', function(event, imdbID) {
+    getMovieReviews(imdbID)
+  });
+
+  $scope.$on('newLogin', function(event, currentUser) {
+    getUserReviews(currentUser)
+  })
 
   // index
   function getAllReviews() {
     $http.get(`${server}/reviews`)
-      .then(function(reviewResponse) {
-        console.log(reviewResponse)
-        console.log(reviewResponse.data)
-        self.allReviews = reviewResponse.data;
+      .then(function(response) {
+        self.allReviews = response.data.reviews;
+        self.posters = response.data.posters;
+      })
+      .then(function(){
+        self.allReviews.forEach(function(review){
+          review.poster = self.posters[review.id]
+        })
       });
   }
   getAllReviews();
+
+  function getMovieReviews(imdbID){
+    $http.get(`${server}/movies/${imdbID}/reviews`)
+    .then(function(response){
+      self.movieReviews = response.data.reviews
+    })
+  }
+
+  function getUserReviews(currentUser){
+    $http.get(`${server}/users/${currentUser.id}/reviews`)
+    .then(function(response){
+      self.userReviews = response.data.reviews
+    })
+  }
 
   self.newReview = {title: '', rating: '', comments: ''};
 
   function addReview(currentUser, movie){
     $http.post(`${server}/movies`, {movie: movie, imdbID: movie.imdbID})
     .then(function(response){
-      console.log(response.data)
       self.id = (response.data.movie.id)
 
-      $http.post(`${server}/movies/${self.id}/reviews`, { review: { title: self.newReview.title, rating: self.newReview.rating, comments: self.newReview.comments, user_id: currentUser.id, movie_id: self.id }})
+      $http.post(`${server}/movies/${self.id}/reviews`, { review: { title: self.newReview.title, rating: self.newReview.rating, comments: self.newReview.comments, user_id: currentUser.id, movie_id: self.id, imdbID: movie.imdbID }})
       .then(function(response){
-        console.log(response);
-        // getMovieReviews(imdbID);
-        // self.newReview = {title: '', rating: '', comments: ''};
+        getMovieReviews(movie.imdbID);
+        self.newReview = {title: '', rating: '', comments: ''};
       })
     })
   }
 
-  function editReview(review) {
-    console.log("Hit Edit Review Route");
-    console.log(review.id)
+  function editReview(review, currentUser) {
     $http.put(`${server}/reviews/` + review.id, { review: { title: self.updatedReview.title, rating: self.updatedReview.rating, comments: self.updatedReview.comments }})
       .then(function(response) {
-        console.log(review)
-        console.log(response)
         self.updatedReview = '';
-        getAllReviews();
+        getUserReviews(currentUser)
       });
   }
 
   function deleteReview(review, currentUser) {
-    console.log("Hit Delete Review Route");
     $http.delete(`${server}/reviews/` + review.id)
       .then(function(response) {
-        getAllReviews();
+        getUserReviews(currentUser);
       });
   }
 
